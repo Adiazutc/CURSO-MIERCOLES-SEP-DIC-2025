465f36bca413d2aa9b9062699878a3360f2a7ba1
Jesus Flores <addevel@outlook.com>
2025-10-22 19:44:07 -0600

    Jesus Flores subió su componente

---
+++ angular-app/src/app/adiaz-component/adiaz-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adiaz-component',
+  imports: [],
+  templateUrl: './adiaz-component.component.html',
+  styleUrl: './adiaz-component.component.css'
+})
+export class AdiazComponentComponent {
+
+}
